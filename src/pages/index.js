9bc1426c75b8332886d38eb0de84511f8a49baca
Paul K. Lynch <drama@DESKTOP-LAIHNG8.corp.nextdigital.com>
2018-04-19 17:16:10 +1000

    Updates to layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <article>
+  <article className="content">
     <section>
       About
     </section>
@@ -13,6 +13,12 @@ const IndexPage = () => (
 
     <section>
       Info
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.962019525125!2d144.95957241580814!3d-37.767488739170915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad6434d28c5ce7b%3A0xb1fec0938929d909!2sSoma!5e0!3m2!1sen!2sau!4v1524118262628"
+        width='600'
+        height='450'
+        frameBorder='0' 
+        style={{ border:0 }} />
     </section>
   </article>
 )

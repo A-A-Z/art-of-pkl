6dbb54790d50c27683540eac02ac5a08b423f7a4
A-A-Z <dramatichamster@gmail.com>
2018-04-25 00:08:08 +1000

    Added banner video

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Logo from '../assets/svg/logo_white.svg'
 
+import VideoMp4 from '../assets/video/banner.mp4'
+
 const Banner = () => (
   <div className='hero-banner'>
-    <Logo />
+    <Logo className='banner-title' />
+    <video width='900' height='470' autoPlay loop muted>
+      <source src={VideoMp4} type='video/mp4' />
+      Your browser does not support the video tag.
+    </video>
   </div>
 )
 
